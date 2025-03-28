dc72a1ae1fa7030e5eff9dc39ed83a89f822ac6a
ayhon <ayhonnick@gmail.com>
2025-03-28 11:13:13 +0100

    feat: add script for single file extraction

---
+++ extract.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail # ; set -x # for debugging
+
+DEFINITIONS_DIR="lean/Shars/Definitions"
+
+charon(){
+    nix run github:aeneasverif/aeneas#charon -L \
+        -- \
+        rustc \
+        --input "src/$1.rs" \
+        --dest-file="$1.llbc" \
+        --hide-marker-traits \
+        --remove-associated-types="*" \
+        -- \
+        --crate-type=lib
+}
+
+aeneas(){
+    nix run github:aeneasverif/aeneas -L -- -backend lean "$1.llbc"
+    rm "$1.llbc"
+}
+
+prog="$0"
+cmd="$1"; shift
+case $cmd in
+
+  "simple")
+      charon "simple"
+      aeneas "simple"
+      mv Simple.lean "$DEFINITIONS_DIR/"
+      rm simple.llbc
+    ;;
+
+  "custom")
+      charon $1
+      aeneas $1
+    ;;
+
+  *)
+      echo "Unknown option, look at $prog to see options available"
+      read && [ -n "$REPLY" ] && less $prog
+      exit 1
+    ;;
+esac
+
