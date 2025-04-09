404bc0ff6cf8aff153c9d240eb13e53369d656a9
ayhon <ayhonnick@gmail.com>
2025-04-09 15:33:15 +0200

    feat: verify pi

--- lean/Shars/Verification/Simple.lean
+++ lean/Shars/Verification/Simple.lean
@@ -877,6 +877,128 @@ theorem simple.rho.spec(input: simple.StateArray)
   progress as ⟨res, res_post⟩
   simp [res_post, Spec.Keccak.ρ.loop.spec]
 
+#print Spec.Keccak.π
+#print simple.pi_loop
+#print simple.pi.inner_loop
+#print simple.pi.inner.inner_loop
+
+@[progress]
+theorem simple.pi.inner.inner_loop.spec(res input : simple.StateArray) (x y z : Std.Usize)
+: x.val < 5
+→ y.val < 5
+→ z.val <= Spec.w 6
+→ ∃ output,
+  inner_loop res input x y z = .ok output ∧
+  ∀ (x' y': Fin 5)(z': Fin (Spec.w 6)),
+    output.toSpec.get x' y' z' = 
+      if x.val = x' ∧ y.val = y' ∧ z.val ≤ z'.val then
+        input.toSpec.get (x + 3*y) x z'
+      else
+        res.toSpec.get x' y' z'
+:= by
+  intro x_idx y_idx z_loop_bnd
+  rw [inner_loop]
+  split
+  . let* ⟨ i, i_post ⟩ ← Std.Usize.mul_spec
+    let* ⟨ i1, i1_post ⟩ ← Std.Usize.add_spec
+    let* ⟨ x2, x2_post ⟩ ← Std.Usize.rem_spec
+    let* ⟨ b, b_post ⟩ ← StateArray.index.spec
+    let* ⟨ x_1, x_2, x_post_1, x_post_2 ⟩ ← StateArray.index_mut.spec
+    let* ⟨ z1, z1_post ⟩ ← Std.Usize.add_spec
+    let* ⟨ rest, rest_post ⟩ ← spec
+    intro x' y' z'
+    rw [rest_post]
+    simp [*]
+    split_all
+    · rfl
+    · scalar_tac
+    · simp [*, ‹z.val = z'.val›']
+      congr 1
+      apply Fin.eq_of_val_eq
+      scalar_tac
+    · rw [Spec.Keccak.StateArray.get_set_neq]
+      simp
+      intro xx' yy' zz'
+      scalar_tac
+  case isFalse z_oob =>
+    simp; intros; simp_ifs
+termination_by Spec.w 6 - z.val
+decreasing_by scalar_decr_tac
+      
+@[progress]
+theorem simple.pi.inner_loop.spec(input res : simple.StateArray) (x y: Std.Usize)
+: x.val < 5
+→ y.val <= 5
+→ ∃ output,
+  inner_loop res input x y = .ok output ∧
+  ∀ (x' y': Fin 5)(z': Fin (Spec.w 6)),
+    output.toSpec.get x' y' z' = 
+      if x.val = x' ∧ y.val ≤ y' then
+        input.toSpec.get (x' + 3*y') x' z'
+      else
+        res.toSpec.get x' y' z'
+:= by
+  intro x_lt y_loop_bnd
+  rw [inner_loop]
+  split
+  . let* ⟨ res1, res1_post ⟩ ← inner.inner_loop.spec
+    let* ⟨ y1, y1_post ⟩ ← Std.Usize.add_spec
+    let* ⟨ rest, rest_post ⟩ ← spec
+    simp [y1_post, rest_post]
+    intro x' y' z'
+    split_all
+    · rfl
+    · scalar_tac
+    · simp [‹y.val = y'.val›', *]
+    · simp [*]
+      intro xx' yy'
+      scalar_tac
+  . simp
+    simp_ifs [implies_true]
+termination_by 5 - y.val
+decreasing_by scalar_decr_tac
+
+@[progress]
+theorem simple.pi_loop.spec(input res : simple.StateArray) (x : Std.Usize)
+: ∃ output,
+  simple.pi_loop input res x = .ok output ∧
+  ∀ (x' y': Fin 5)(z': Fin (Spec.w 6)),
+    output.toSpec.get x' y' z' = 
+      if x.val ≤ x' then
+        input.toSpec.get (x' + 3*y') x' z'
+      else
+        res.toSpec.get x' y' z'
+:= by
+  rw [pi_loop]
+  progress*
+  · intro x' y' z'
+    simp [res_post]
+    split_all
+    · rfl
+    · scalar_tac
+    · simp [‹x.val = x'.val›', *]
+    · simp [*]; intro; scalar_tac
+  · simp
+    simp_ifs
+    simp only [implies_true]
+termination_by 5 - x.val
+decreasing_by scalar_decr_tac
+
+@[progress]
+theorem simple.pi.spec(input: simple.StateArray)
+: ∃ output,
+  pi input = .ok output ∧
+  output.toSpec = Spec.Keccak.π input.toSpec
+:= by
+  simp [pi, Spec.Keccak.π, ClonesimpleStateArray.clone]
+  let* ⟨ res, res_post ⟩ ← pi_loop.spec
+  ext x' y' z'
+  simp [res_post]
+
+theorem simple.IOTA_RC_POINTS.spec
+: ∀ t < 255, IOTA_RC_POINTS.val[t]! = Spec.Keccak.ι.rc t
+:= by native_decide
+
 theorem Array.getElem?_chunks_exact(k: Nat)(arr: Array α)(i: Nat)
 : i < arr.size / k
 → Vector.toArray <$> (arr.chunks_exact k)[i]? = some (arr.extract (k*i) (k*(i+1)))
