abf136f4eefb1ad7832f6b19f78a7b93dcbcc928
ayhon <ayhonnick@gmail.com>
2025-04-21 13:00:31 +0200

    wip: sponge_absorb_final with core.slice.index.Slice.index not unfolded

--- lean/Shars/Verification/Simple/Sponge.lean
+++ lean/Shars/Verification/Simple/Sponge.lean
@@ -89,6 +89,67 @@ theorem BitVec.cast_xor(bv bv2: BitVec n)(eq: n = m)
 : (bv ^^^ bv2).cast eq = bv.cast eq ^^^ bv2.cast eq
 := by ext i i_lt; simp
 
+def absorb' (s: List Bool) (chunks: List (List Bool)): List Bool := Id.run do
+  let mut s := s
+  for Pi in chunks do
+    s := s.zipWith (· ^^ ·) Pi ++ s.drop Pi.length
+  return s
+
+
+def List.chunks_exact(k: Nat)(ls: List α): List (List α) :=
+  if ls.length < k ∨ k = 0 then
+    return []
+  else
+    let chunk := (ls.take k)
+    let rest := (ls.drop k).chunks_exact k
+    chunk :: rest
+termination_by ls.length
+decreasing_by simp_wf; omega
+
+@[progress]
+theorem Aeneas.Std.Array.to_slice_mut.spec{α : Type u} {n : Usize} (a : Array α n) 
+: ∃ old new,
+  Std.toResult (a.to_slice_mut) = .ok (old, new) ∧
+  old = a.to_slice ∧
+  new = a.from_slice
+:= by simp [to_slice_mut, Std.toResult]
+
+/- set_option maxHeartbeats 1000000 in -/
+/- @[progress] -/
+/- theorem simple.sponge_absorb_initial_loop.spec' -/
+/-   (bs : Std.Slice Bool) (r : Std.Usize) (s : Aeneas.Std.Array Bool 1600#usize) (n i : Std.Usize) -/
+/- : n.val = bs.length / r.val -/
+/- → (r_pos: r.val > 0) -/
+/- → i.val ≤ n.val -/
+/- → r.val < 1600 -/
+/- → ∃ output, -/
+/-   sponge_absorb_initial_loop bs r s n i = .ok output ∧ -/ 
+/-   output.val = (absorb' (s.val) (bs.val.chunks_exact r |>.drop i)) -/
+/- := by -/
+/-   rintro n_val r_pos i_loop_bnd r_le -/
+/-   rw [sponge_absorb_initial_loop] -/
+/-   split -/
+/-   case isTrue i_idx => -/
+/-     have _: r.val*i.val ≤ bs.length := sorry -- r * i < r * n = r * (bs.length / r) ≤ bs.length ≤ Usize.max -/
+/-     have _: r.val*(i.val + 1) ≤ bs.length := sorry -- r * (i + 1) ≤ r * n = r * (bs.length / r) ≤ bs.length ≤ Usize.max -/
+
+/-     let* ⟨ i1, i1_post ⟩ ← Std.Usize.mul_spec -/
+/-     let* ⟨ i2, i2_post ⟩ ← Std.Usize.add_spec -/
+/-     let* ⟨ i3, i3_post ⟩ ← Std.Usize.mul_spec -/
+/-     let* ⟨ chunk, chunk_post ⟩ ← Std.core.slice.index.Slice.index.slice_index_range_usize_slice_spec -/
+/-     let* ⟨ old_slice, new_slice, old_slice_post, new_slice_post ⟩ ← Std.Array.to_slice_mut.spec -/
+/-     let* ⟨ s2, s2_post_1, s2_post_2 ⟩ ← xor_long.spec' -/
+/-     let* ⟨ s4, s4_post ⟩ ← keccak_p.spec' -/
+/-     let* ⟨ res, res_post ⟩ ← spec' -/
+    
+/-     simp [*, absorb'] -/
+/-     rw [←List.cons_getElem_drop_succ (n := i.val)] -/
+/-     simp -/
+/-   . sorry -/
+/-   -- TODO -/
+
+
+attribute [simp] Std.Slice
 set_option maxHeartbeats 1000000 in
 @[progress]
 theorem simple.sponge_absorb_initial_loop.spec
@@ -115,17 +176,22 @@ theorem simple.sponge_absorb_initial_loop.spec
     let* ⟨ slice, slice_post ⟩ ← Std.core.slice.index.Slice.index.slice_index_range_usize_slice_spec
     fsimp only [*] at slice_post
     let* ⟨old_slice, new_slice, slice_mut_post⟩ ← Std.Array.to_slice_mut.progress_spec
+
     simp [Std.Array.to_slice_mut, Std.Array.to_slice] at slice_mut_post
     obtain ⟨old_slice_post, new_slice_post⟩ := slice_mut_post
+
     let* ⟨ s2, s2_len, s2_post ⟩ ← xor_long.spec
     rw [old_slice_post] at s2_post
     simp [Std.Slice.toBitVec, s2_len] at s2_post
+
     rw [slice_post] at s2_post
-    /- rw [s2_len] at s2_post -/ -- NOTE: Annoying that this doesn't work
+    simp at s2_len
+    /- rw [s2_len] at s2_post -/ -- NOTE: Annoying that this doesn't work (setWidth's type depends on argument)
+
     let* ⟨ s4, s4_post ⟩ ← keccak_p.spec
     let* ⟨rest, rest_post ⟩ ← spec
-
-    simp [*, new_slice_post]
+    
+    simp [*]
     apply congrArg
     have := @absorb_def r.val (Spec.Keccak.P 6 24 (BitVec.cast (by simp [Spec.w, Spec.b]) (s.from_slice s2).toBitVec))
     simp [←Array.map_extract]
@@ -173,12 +239,15 @@ theorem simple.sponge_absorb_initial.spec
   let* ⟨ res, res_post ⟩ ← simple.sponge_absorb_initial_loop.spec
   simp [*, Array.extract_eq_self_of_le]
 
+attribute [progress_pure_def] Aeneas.Std.core.num.Usize.saturating_sub 
+
 @[progress]
 theorem simple.sponge_absorb_final.spec
   (rest suffix : Std.Slice Bool) (r : Std.Usize) (s : Aeneas.Std.Array Bool _)
 (r_pos: r.val > 0)
 : rest.length < r
 → suffix.length ≤ 4
+→ rest.length + suffix.length < Std.Usize.max
 → ∃ output,
   sponge_absorb_final s rest suffix r = .ok output ∧ 
   let h := by simp [Spec.b, Spec.w]
@@ -189,8 +258,46 @@ theorem simple.sponge_absorb_final.spec
     )
   ).cast h.symm
 := by 
-  intro rest_small suffix_le
-  sorry
+  intro rest_small suffix_le no_overflow
+  rw [sponge_absorb_final]
+
+  let* ⟨ nb_left, nb_left_post ⟩ ← Std.Usize.add_spec
+  let* ⟨ x_1, x_2, x_post_1, x_post_2 ⟩ ← Std.Array.to_slice_mut.spec
+  let* ⟨ s2, s2_post_1, s2_post_2 ⟩ ← xor_long.spec
+  let* ⟨_, _⟩ ← Std.Array.to_slice_mut.progress_spec
+  let* ⟨ s5, s5_post_1, s5_post_2 ⟩ ← xor_long_at.spec
+  let* ⟨leftover, leftover_post⟩ ← Std.core.num.Usize.saturating_sub.progress_spec
+  split
+  case isTrue leftover_pos =>
+    let* ⟨ s7, s7_post ⟩ ← keccak_p.spec'
+    let* ⟨ x_1, x_2, x_post_1, x_post_2 ⟩ ← Std.Array.to_slice_mut.spec
+    sorry
+  case isFalse leftover_zero =>
+    simp at leftover_zero
+    let* ⟨ x_1, x_2, x_post_1, x_post_2 ⟩ ← Std.Array.to_slice_mut.spec
+    let* ⟨ s8, s8_post ⟩ ← Std.Array.to_slice.progress_spec
+    let* ⟨ s9, s9_post_1, s9_post_2 ⟩ ← xor_long_at.spec
+    · sorry
+    let* ⟨ i3, i3_post ⟩ ← Std.Usize.add_spec
+    split
+    . let* ⟨ x_1, x_2, x_post_1, x_post_2 ⟩ ← Std.Array.to_slice_mut.spec
+      let* ⟨ i5, i5_post ⟩ ← Std.Usize.sub_spec
+      let* ⟨ s18, s18_post_1, s18_post_2 ⟩ ← xor_long_at.spec
+      · sorry
+      let* ⟨ res, res_post ⟩ ← keccak_p.spec'
+    . let* ⟨ s11, s11_post ⟩ ← keccak_p.spec'
+      let* ⟨ x_1, x_2, x_post_1, x_post_2 ⟩ ← Std.Array.to_slice_mut.spec
+      let* ⟨ i5, i5_post ⟩ ← Std.Usize.sub_spec
+      let* ⟨ s18, s18_post_1, s18_post_2 ⟩ ← xor_long_at.spec
+      · sorry
+      let* ⟨ res, res_post ⟩ ← keccak_p.spec'
+  
+
+  generalize Std.core.num.Usize.saturating_sub nb_left r = leftover
+  fsimp [Std.toResult]
+
+  /- progress*? -/
+  
 
 
 /- @[progress] -/
@@ -205,7 +312,7 @@ theorem simple.sponge_absorb.spec
   intro r_bounded
   unfold sponge_absorb
   let* ⟨_, _⟩ ← simple.sponge_absorb_initial.spec
-  progress*?
+  /- progress*? -/
 
   sorry
 
