6fc2ce1b2fb7ec021510843def10d0becd47f1ff
ayhon <ayhonnick@gmail.com>
2025-05-28 11:36:43 +0200

    feat: keccak permutation verified

--- lean/Shars/Verification/KeccakP.lean
+++ lean/Shars/Verification/KeccakP.lean
@@ -7,7 +7,7 @@ import Shars.Verification.Rho
 import Shars.Verification.Pi
 import Shars.Verification.Chi
 import Shars.Verification.Iota
-import Shars.Verification.ListIR
+/- import Shars.Verification.ListIR -/
 
 set_option maxHeartbeats 10000000
 attribute [-simp] List.getElem!_eq_getElem?_getD
@@ -35,63 +35,34 @@ def Spec.Keccak.P.loop(l: Fin 7)(nᵣ: Nat)(S: StateArray l)(start: Nat) := Id.r
     A := Rnd A iᵣ
   return A
 
-def Spec.Keccak.P.loop.refinement(l: Fin 7)(nᵣ: Nat)(S: BitVec (Spec.b l))
-: P  l nᵣ S = (P.loop l nᵣ (StateArray.ofBitVec S) 0).toBitVec
+def Spec.Keccak.P.loop.refinement(l: Fin 7)(nᵣ: Nat)(S: Bitstring (Spec.b l))
+: P  l nᵣ S = (P.loop l nᵣ (StateArray.ofVector S) 0).toVector
 := by congr
 
-
 @[progress]
-theorem algos.keccak_p_aux_loop.spec(input: StateArray)(ir: Std.Usize)
+theorem algos.keccak_p_loop.spec(input: StateArray)(ir: Std.Usize)
 : ir.val ≤ 24
 → ∃ output,
-  keccak_p_aux_loop input ir = .ok output ∧
-  output.toSpec = Spec.Keccak.P.loop 6 24 input.toSpec ir.val
+  keccak_p_loop input ir = .ok output ∧
+  output.toBits = (Spec.Keccak.P.loop 6 24 input.toSpec ir.val).toBits
 := by
   intro ir_loop_bound
-  rw [keccak_p_aux_loop]
-  split
-  case isTrue ir_lt =>
-    let* ⟨ a1, a1_post ⟩ ← round.spec
-    let* ⟨ ir1, ir1_post ⟩ ← Std.Usize.add_spec
-    let* ⟨ res, res_post ⟩ ← spec
-
-    simp +arith [*, Spec.Keccak.P.loop, SRRange.foldl_range'_eq_foldWhile]
-    conv => rhs; rw [SRRange.foldWhile_step (h := by simpa using ir_lt)]
+  rw [keccak_p_loop]
+  progress*
+  · simp +arith [*, Spec.Keccak.P.loop, SRRange.foldl_range'_eq_foldWhile]
+    conv => rhs; rw [SRRange.foldWhile_step (h := by scalar_tac)]
     congr
     scalar_tac
-  case isFalse ir_oob =>
-    simp [‹ir.val = 24›', Spec.Keccak.P.loop]
+  · simp +decide [‹ir.val = 24›', Spec.Keccak.P.loop, Spec.Keccak.StateArray.toBits_toStateArray]
 termination_by 24 - ir.val
 decreasing_by scalar_decr_tac
 
-theorem Spec.Keccak.StateArray.ofBitVec_inj(bv bv' : BitVec (Spec.b l))
-: bv = bv' ↔ StateArray.ofBitVec bv = StateArray.ofBitVec bv'
-:= by apply Iff.intro <;> intro h <;> simpa using h
-
-theorem Spec.Keccak.StateArray.ofBitVec_toBitVec(S: StateArray l)
-: { toBitVec := S.toBitVec } = S
-:= by simp
-
 @[progress]
-theorem algos.keccak_p.spec(input: Aeneas.Std.Array Bool 1600#usize)
+theorem algos.keccak_p.spec(input: StateArray)
 : ∃ output,
   keccak_p input = .ok output ∧
-  output.val.toBitVec.cast (by simp [Spec.b, Spec.w]) = Spec.Keccak.P 6 24 (input.val.toBitVec.cast (by simp [Spec.b, Spec.w]))
+  output.toBits = (Spec.Keccak.P 6 24 (input.toSpec.toVector)).toList
 := by
-  rw [keccak_p, keccak_p_aux]
+  rw [keccak_p]
   progress as ⟨res, res_post⟩
-  replace res_post := congrArg (f := Spec.Keccak.StateArray.toBitVec) res_post
-  simp [StateArray.toSpec] at res_post
-  simp [Spec.Keccak.P.loop.refinement, res_post]
-
-@[progress]
-theorem algos.keccak_p.spec'(input: Aeneas.Std.Array Bool 1600#usize)
-: ∃ output,
-  keccak_p input = .ok output ∧
-  output.val = ListIR.list_keccak_p input.val
-:= by
-  rw [keccak_p, keccak_p_aux]
-  progress as ⟨res, res_post⟩
-  replace res_post := congrArg (f := BitVec.toList ∘ Spec.Keccak.StateArray.toBitVec) res_post
-  simp [StateArray.toSpec] at res_post
-  simp [*, ListIR.list_keccak_p, Spec.Keccak.P.loop.refinement, BitVec.setWidth_eq_cast, Spec.b, Spec.w, res_post]
+  simp [res_post, Spec.Keccak.P.loop.refinement, res_post]
