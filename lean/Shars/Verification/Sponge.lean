988de179c2a4dddfccab23cafbb7835f34d34352
ayhon <ayhonnick@gmail.com>
2025-06-12 10:00:55 +0200

    wip: sponge

--- lean/Shars/Verification/Sponge.lean
+++ lean/Shars/Verification/Sponge.lean
@@ -11,7 +11,7 @@ import Init.Data.Array
 import Shars.Verification.Utils
 import Shars.Verification.Refinement
 import Shars.Verification.Auxiliary
-import Shars.Verification.ListIR
+-- import Shars.Verification.ListIR
 import Shars.Verification.SpongeAbsorb
 import Shars.Verification.SpongeSqueeze
 
@@ -25,45 +25,91 @@ theorem Nat.cast_mod_Int(a b: Nat)
 : ((a % b).cast: ℤ) = a.cast % b.cast
 := by simp [Nat.cast]
 
-set_option maxRecDepth 1000000 in
-@[progress]
-theorem algos.sponge.spec(r : Std.Usize) (bs dst suffix : Std.Slice Bool)
-: (r_big_enough: r.val ≥ 6)
-→ suffix.length ≤ 4
-→ r.val < 1600
--- → dst.length > 0
+-- set_option diagnostics true in
+set_option maxRecDepth 7500 in
+set_option maxHeartbeats 1000000 in
+-- @[progress]
+theorem algos.sponge.spec(r : Std.Usize) (bs input : Std.Slice Std.U8)(extra: Std.U8)
+  (border: { i // i < 7 })
+  (border_spec: extra.toBits[border.val]! = true ∧ ∀ j, border.val < j → extra.toBits[j]! = false)
+: (r_pos: 0 < r.val)
+→ (r_bnd: r.val < 200)
+→ 8 ∣ r.val
 → dst.length + r.val < Std.Usize.max
 → ∃ output,
-  sponge r bs dst suffix = .ok output ∧
-  have: NeZero r.val := {out:=Nat.ne_zero_of_lt r_big_enough}
-  output.val = (Spec.sponge (f := Spec.Keccak.P 6 24) Spec.«pad10*1» r (bs.val ++ suffix.val).toArray dst.length).toList
+  sponge r bs dst extra = .ok output ∧
+  output.toBits = (Spec.sponge
+    (f := Spec.Keccak.P 6 24)
+    (pad := Spec.«pad10*1»)
+    (r := ⟨8*r, by simp [*, Spec.b, Spec.w]; omega⟩)
+    (bs.toBits ++ extra.toBits.take border).toArray
+    dst.toBits.length
+   ).toList
 := by
   intros
-  rw [sponge]
-  have: NeZero r.val := {out:=Nat.ne_zero_of_lt (by assumption)}
-  progress with algos.sponge_absorb.spec_list as ⟨mid, mid_post⟩
-  · simp [Std.Array.repeat]
-  progress with algos.sponge_squeeze.spec as ⟨res, post⟩
-  simp [*]
-  simp [Spec.sponge]
-  have absorb_spec := absorb.spec
-  simp [spec_sponge_absorb] at absorb_spec
-  rw [ListIR.sponge.squeeze.refinement, absorb_spec, absorb.refinement]
-  case r_pos => scalar_tac
-  simp
-  congr 6
-  unfold Spec.«pad10*1»
-  simp
-  congr 2
-  ext i i_lhs i_rhs
-  · simp [BitVec.toNat]
-    congr 1
-    -- apply Int.ModEq.eq
-    ring_nf
-    rw [←Int.add_emod_emod]
-    rw [Int.sub_emod_emod]
-    rw [Int.add_emod_emod]
-  simp [BitVec.toArray]
+  obtain ⟨getElem!_border, getElem!_of_border_lt⟩ := border_spec
+  have border_le_8: border.val ≤ 8 := by omega
+  have: extra.toBits.take border ++ [true] ++ List.replicate (8 - border - 1) false = extra.toBits := by
+    apply List.ext_getElem <;> simp +decide [*, ←getElem!_pos, ‹border.val + (8 - border.val - 1 + 1) = 8›']
+    intro i i_lt
+    if i < border then
+      rw [List.getElem!_append_left]; case h => simp; omega
+      rw [List.getElem!_take_of_lt]; case x => omega
+    else if i = border then
+      simp [*]
+    else
+      simp [getElem!_of_border_lt, ‹border < i›']
+      rw [List.getElem!_append_right]; case h => simp; omega
+      simp [border_le_8]
+      obtain ⟨m', m'_val⟩ := Nat.exists_eq_add_one.mpr ‹0 < i - border.val›'
+      simp [m'_val]
+      rw [List.getElem!_replicate]; case h => omega
+  have last_extra_eq_false: extra.toBits[7]! = false := getElem!_of_border_lt 7 border.property
+  unfold sponge sponge_absorb
+  simp [DefaultalgosStateArray.default, Std.core.slice.index.Slice.index]
+  have: r.val * (bs.val.length / r.val) ≤ bs.length := by scalar_tac
+  have: bs.length ≤ Std.Usize.max := bs.property
+
+  progress*? by simp [*, ←Nat.mod_eq_sub, Nat.mod_lt]; try omega
+  progress with algos.sponge_squeeze.spec as ⟨res, res_post⟩
+
+  simp [*, Spec.sponge, IR.squeeze.refinement]
+
+  -- TODO: Make this nicer, IR.absorb.refinement should be enough
+  rw [IR.array_absorb.refinement]
+  case r_pos => omega
+  case r_bnd => simp [Spec.b, Spec.w]; omega
+  rw [IR.absorb.refinement, IR.absorb.unfold]
+  case r_pos => omega
+
+  simp [*, Array.toList_chunks_exact, List.append_assoc]
+  congr
+  rw [←Nat.mod_eq_sub]
+  sorry
+
+  -- progress with algos.sponge_absorb.spec_list as ⟨mid, mid_post⟩
+  -- · simp [Std.Array.repeat]
+  -- progress with algos.sponge_squeeze.spec as ⟨res, post⟩
+  -- simp [*]
+  -- simp [Spec.sponge]
+  -- have absorb_spec := absorb.spec
+  -- simp [spec_sponge_absorb] at absorb_spec
+  -- rw [ListIR.sponge.squeeze.refinement, absorb_spec, absorb.refinement]
+  -- case r_pos => scalar_tac
+  -- simp
+  -- congr 6
+  -- unfold Spec.«pad10*1»
+  -- simp
+  -- congr 2
+  -- ext i i_lhs i_rhs
+  -- · simp [BitVec.toNat]
+  --   congr 1
+  --   -- apply Int.ModEq.eq
+  --   ring_nf
+  --   rw [←Int.add_emod_emod]
+  --   rw [Int.sub_emod_emod]
+  --   rw [Int.add_emod_emod]
+  -- simp [BitVec.toArray]
 
 
 attribute [local simp] Std.Array.make Std.Array.to_slice Std.Array.from_slice Std.Array.length Std.Array.repeat
