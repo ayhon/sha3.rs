d1ec640bef29c366c5c9e35f73e02670f60521c0
ayhon <ayhonnick@gmail.com>
2025-07-02 14:37:40 +0200

    feat: update implementation of CLI binary

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,43 @@
-use sha3::{self, get_vec_of_bits, hex_of_vec_of_bits};
+use shars::{algos::*, hex_of_vec_of_bits, decompress_u8};
 
 fn main() -> std::io::Result<()> {
     let mut iter = std::env::args();
     let _exe = iter.next();
-    // println!("args");
-    // for arg in std::env::args() {
-    //     println!("{}", arg);
-    // }
-    // println!("args done");
     loop {
         let Some(op) = iter.next() else {break};
         let msg = iter.next().unwrap_or("".to_string());
-        let bs: Vec<bool> = get_vec_of_bits(&msg);
+        let bs: Vec<u8> = msg.bytes().collect();
         let output = match op.as_str() {
             "SHA3_224" => {
-                sha3::simple::sha3_224(&bs).to_vec()
+                sha3_224(&bs).to_vec()
             }
             "SHA3_256" => {
-                sha3::simple::sha3_256(&bs).to_vec()
+                sha3_256(&bs).to_vec()
             }
             "SHA3_384" => {
-                sha3::simple::sha3_384(&bs).to_vec()
+                sha3_384(&bs).to_vec()
             }
             "SHA3_512" => {
-                sha3::simple::sha3_512(&bs).to_vec()
+                sha3_512(&bs).to_vec()
             }
             "SHAKE128" => {
                 let Some(Ok(d)) = iter.next().map(|x| usize::from_str_radix(&x, 10)) else {break};
-                let mut output = Vec::new(); for _ in 0..d { output.push(false); }
-                sha3::simple::shake128(&bs, &mut output);
+                let mut output = vec![0; d];
+                shake128(&bs, &mut output);
                 output.to_vec()
             }
             "SHAKE256" => {
                 let Some(Ok(d)) = iter.next().map(|x| usize::from_str_radix(&x, 10)) else {break};
-                let mut output = Vec::new(); for _ in 0..d { output.push(false); }
-                sha3::simple::shake256(&bs, &mut output);
+                let mut output = vec![0; d];
+                shake256(&bs, &mut output);
                 output.to_vec()
             }
-            _ => break
+            _ => {
+                eprint!("Unrecognized option: {}", &op);
+                break
+            }
         };
-        println!("{}", hex_of_vec_of_bits(&output));
+        println!("{}", hex_of_vec_of_bits(&decompress_u8(&output)));
     }
     return Ok(())
 }
